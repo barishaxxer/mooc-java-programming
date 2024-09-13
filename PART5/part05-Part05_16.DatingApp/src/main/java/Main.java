34cbacf7a0ea5f297dec50de776f6088c8ca49e1
barishaxxer <bariscankaxxer@gmail.com>
2024-09-13 19:31:27 +0300

    Add PART5

--- PART5/part05-Part05_16.DatingApp/src/main/java/Main.java
+++ PART5/part05-Part05_16.DatingApp/src/main/java/Main.java
@@ -3,5 +3,8 @@ public class Main {
 
     public static void main(String[] args) {
         // test your program here!
+        SimpleDate test = new SimpleDate(14, 9, 2005);
+        test.advance();
+        test.advance();
     }
 }
--- PART5/part05-Part05_17.Money/src/main/java/Money.java
+++ PART5/part05-Part05_17.Money/src/main/java/Money.java
@@ -31,5 +31,37 @@ public class Money {
 
         return this.euros + "." + zero + this.cents + "e";
     }
+    public Money plus(Money addition){
+        Money xd = new Money(this.euros + addition.euros, this.cents + addition.cents);
+        return xd;
+    }
+    public boolean lessThan(Money compared){
+        if (this.euros < compared.euros) {
+            return true;
+            
+        }else if (this.euros == compared.euros && this.cents < compared.cents) {
+            return true;
+            
+            
+        }
+        return false;
+    }
+    public Money minus(Money decreaser){
+        
+        double comparing = this.euros * 100 + this.cents;
+        double compared = decreaser.euros * 100 + decreaser.cents;
+        if (comparing - compared < 0) {
+              return new Money(0, 0);
+            
+        }
+        double total = 1.0 * (comparing - compared) / 100;
+        String formatted = String.format("%.2f", total);
+        String[] parts = String.valueOf(formatted).split("\\.");
+        Money clone = new Money(Integer.valueOf(parts[0]), Integer.valueOf(parts[1]));
+        return clone;
+        
+       
+    }
+    
 
 }
